b7dfca9f3bf3d90be9af543c402473e0d4eda584
ani294 <aniruddha.294@gmail.com>
2023-03-25 20:44:27 +0530

    1.0.1 - Code Rechecked & Updated

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# Project - Countdown App
+
+![Project-Image](/Asset/proj-img.jpg)
+
+## Project Details :
+
+_Whenever a user wants to store something in the clipboard, he enters in the textbox and on clicking on submit button, it will save in the clipboard._
+
+- Project Live Link : [https://count-down-app-vanilla-js.vercel.app/](https://count-down-app-vanilla-js.vercel.app/)
+
+### Technology Used -
+
+- HTML
+- CSS
+- Vanilla Javascript
+
+### Key Learnings from the project -
+
+1. Project Planning & its Execution
+2. Key Project Structure & its elements
+3. HTML Structure to invok JS code
+4. Various usage of CSS and its properties
+5. Key Elements design in CSS to populate with JS invoke
+6. Writing code block by block to build overall project
+7. Vanilla JS execution on elements to execute step-by-step
+8. Advanced DOM Manipulation
+9. Project Testing
+10. Github & Vercel utilisation
+
+- Project Live Link : [https://count-down-app-vanilla-js.vercel.app/](https://count-down-app-vanilla-js.vercel.app/)
+
+### - Developed by [Aniruddha](https://github.com/AniruddhaDas1) | CoderX Community | [Send Message to WhatsApp](https://wa.me/9123987124)
--- script.js
+++ script.js
@@ -4,37 +4,34 @@ document.getElementById("end-date").innerHTML = endDate;
 
 const inputs = document.querySelectorAll("input");
 
-
 const clock = () => {
-    const end = new Date(endDate);
-    const now = new Date();
+  const end = new Date(endDate);
+  const now = new Date();
 
-    // end - now = gives miliseconds, divided by 1000 gives Seconds.
-    const diff = (end - now) / 1000;
+  // end - now = gives miliseconds, divided by 1000 gives Seconds.
+  const diff = (end - now) / 1000;
 
-    // Negative value will not return
-    if(diff < 0) return;
+  // Negative value will not return
+  if (diff < 0) return;
 
-    //convert into days & shows on input value
-     inputs[0].value = Math.floor(diff/3600/24);
-     // Hours
-     inputs[1].value = Math.floor((diff/3600) % 24);
-     //Minutes
-     inputs[2].value = Math.floor((diff/60) % 60);
-     //
-     inputs[3].value = Math.floor(diff) % 60;
-
-}
+  //convert into days & shows on input value
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  // Hours
+  inputs[1].value = Math.floor((diff / 3600) % 24);
+  //Minutes
+  inputs[2].value = Math.floor((diff / 60) % 60);
+  //
+  inputs[3].value = Math.floor(diff) % 60;
+};
 
 clock();
 
-setInterval( () => {
-    clock();
+setInterval(() => {
+  clock();
 }, 1000);
 
-
 /**
-     * 1 day = 24 hrs
-     * 1 hr = 60 mins
-     * 60 min = 3600 sec;
-     */
+ * 1 day = 24 hrs
+ * 1 hr = 60 mins
+ * 60 min = 3600 sec;
+ */
